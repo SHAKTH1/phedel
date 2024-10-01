adca3dcc5d65a71ab17d07afb55ece0f5708a4b4
SHAKTHI <shakthi.amarnath@gmail.com>
2024-10-01 15:12:13 +0530

    LATEST

--- public/js/index.js
+++ public/js/index.js
@@ -87,3 +87,48 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   
 
+  let list = document.querySelector('.banner-slider .banner-list');
+  let items = document.querySelectorAll('.banner-slider .banner-list .banner-item');
+  let dots = document.querySelectorAll('.banner-slider .banner-dots li');
+  let prev = document.getElementById('prev');
+  let next = document.getElementById('next');
+  
+  let active = 0;
+  let lengthitems = items.length - 1;
+  
+  next.onclick = function() {
+      if (active + 1 > lengthitems) {
+          active = 0;
+      } else {
+          active = active + 1;
+      }
+      reloadslider();
+  }
+  
+  prev.onclick = function() {
+      if (active - 1 < 0) {
+          active = lengthitems;
+      } else {
+          active = active - 1;
+      }
+      reloadslider();
+  }
+  
+  let autoslide = setInterval(() => { next.click(); }, 3000);
+  
+  function reloadslider() {
+      let checkleft = items[active].offsetLeft;
+      list.style.left = -checkleft + 'px';
+  
+      let lastactiveDot = document.querySelector('.banner-slider .banner-dots li.active');
+      if (lastactiveDot) lastactiveDot.classList.remove('active');
+      dots[active].classList.add('active');
+  }
+  
+  dots.forEach((li, key) => {
+      li.addEventListener('click', function() {
+          active = key;
+          reloadslider();
+      });
+  });
+  
